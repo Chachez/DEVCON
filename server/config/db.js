da0f66614d1af292f0d806b96eb70478ca66ee88
chachez <chachamichael89@gmail.com>
2022-04-16 14:20:39 +0300

    feat: Connected the databse to the application

---
+++ server/config/db.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+const config = require("config");
+const db = config.get("mongoURI");
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(
+      db
+      /**
+         * Whenever these is an error about URL string parsing resolve with this
+         *{
+             useNewUrlParser:true
+         }
+         */
+    );
+    console.log("Connected to database");
+  } catch (err) {
+    console.log(err.message);
+    // Exiting process on failure
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
