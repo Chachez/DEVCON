848b916aba88ac5c3865b983aa3131e268749a50
Chachez <chachamichael89@gmail.com>
2022-06-03 07:13:33 +0300

    core: Added redux persist

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,17 +1,30 @@
 import { createStore, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
 import rootReducer from './rootReducer';
+
+const persistConfig = {
+  key: 'root',
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 const initialState = {};
 
 const middleware = [thunk];
 
 const store = createStore(
-  rootReducer,
+  persistedReducer,
   initialState,
   composeWithDevTools(applyMiddleware(...middleware))
 );
+
+const persistor = persistStore(store);
+
+export { persistor };
 
 export default store;
